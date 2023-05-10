f0092bb70b89aa0d938ec7fb69afa54fd393d26c
AnmZ <anmit.ray911@gmail.com>
2023-05-10 17:19:58 +0530

    Created a threadPool to limit number of threads and save thread creation time ; ''

--- main.go
+++ main.go
@@ -6,22 +6,31 @@ import (
 	"time"
 )
 
-func serve(conn net.Conn) {
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
+func worker(id int, jobs chan net.Conn) {
+	for {
+		conn := <-jobs
+
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("worker %d: processing the request", id)
+		time.Sleep(8 * time.Second)
+
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+		conn.Close()
 	}
-
-	log.Println("processing the request")
-
-	time.Sleep(8 * time.Second)
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
-	conn.Close()
 }
 
 func main() {
+	jobs := make(chan net.Conn)
+
+	for i := 1; i <= 10; i++ {
+		go worker(i, jobs)
+	}
+
 	listener, err := net.Listen("tcp", ":1729")
 	if err != nil {
 		log.Fatal(err)
@@ -34,6 +43,7 @@ func main() {
 			log.Fatal(err)
 		}
 		log.Println("client connected")
-		go serve(conn)
+
+		jobs <- conn
 	}
 }
