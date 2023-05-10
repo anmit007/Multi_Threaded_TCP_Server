e664c4f7b38f3eae5f5c8185d8177c753be59184
AnmZ <anmit.ray911@gmail.com>
2023-05-10 17:01:07 +0530

    made server always running

--- main.go
+++ main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"time"
 )
 
 func serve(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	time.Sleep(1 * time.Second)
+	log.Println("processing the request")
+
+	time.Sleep(8 * time.Second)
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
 	conn.Close()
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":3000")
+	listener, err := net.Listen("tcp", ":1729")
 	if err != nil {
 		log.Fatal(err)
 	}
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal(err)
+
+	for {
+		log.Println("waiting for a client to connect")
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("client connected")
+
+		serve(conn)
 	}
-	fmt.Println("Hello World", conn)
-	serve(conn)
 }
